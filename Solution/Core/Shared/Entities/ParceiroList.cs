a3121b28b85a64f439c740db62b799c44959f80e
Joao Felix <joao.seixas@jacaptei.com.br>
2024-09-30 17:46:14 -0300

    chore: implementação de model para retorno da lista dos "Parceiros" ativos

--- Solution/Core/Shared/Entities/ParceiroList.cs
+++ Solution/Core/Shared/Entities/ParceiroList.cs
@@ -4,7 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JaCaptei.Model.Entities
+namespace JaCaptei.Model
 {
-
+    public class ParceiroList
+    {
+        public int id { get; set; }
+        public string imobiliaria { get; set; } = "";
+        public string nomeParceiro { get; set; } = "";
+        public string telefoneParceiro { get; set; } = "";
+        public string cpfParceiro { get; set; }
+        public string cnpjParceiro { get; set; }
+    }
 }
